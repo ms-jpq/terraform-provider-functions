3ae408e58ab1adae858448d858606449f68399e9
IIIIII <github@bigly.dog>
2024-08-04 11:00:21 -0400

    11

--- internal/provider.go
+++ internal/provider.go
@@ -33,7 +33,9 @@ func (p *FuncProvider) Metadata(_ context.Context, _ provider.MetadataRequest, r
 
 func (p *FuncProvider) Schema(_ context.Context, _ provider.SchemaRequest, rsp *provider.SchemaResponse) {
 	rsp.Schema = schema.Schema{
-		Attributes: map[string]schema.Attribute{},
+		Attributes: map[string]schema.Attribute{
+			"_": schema.BoolAttribute{},
+		},
 	}
 }
 
